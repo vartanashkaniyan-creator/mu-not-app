d376143414ac115d5016dc1f7f658141a1c9b5e8
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-01 22:33:05 +0330

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,1 +1,18 @@
-
+calculator() {
+  return {
+    ui: `
+      <h2>🧮 Calculator</h2>
+      <input id="a" type="number">
+      <input id="b" type="number">
+      <button onclick="calc()">+</button>
+      <p id="res"></p>
+    `,
+    logic: `
+      function calc() {
+        const aVal = Number(document.getElementById("a").value);
+        const bVal = Number(document.getElementById("b").value);
+        document.getElementById("res").innerText = aVal + bVal;
+      }
+    `
+  };
+}
