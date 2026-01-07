cfff95f6222bd67779022fccff8376f6a1d67f53
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-07 10:33:08 +0330

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,21 @@
+const Storage = {
+    save(key, data) {
+        try {
+            localStorage.setItem(key, JSON.stringify(data));
+        } catch (err) {
+            console.error('خطا در ذخیره‌سازی', err);
+        }
+    },
 
+    load(key, defaultValue = null) {
+        try {
+            const item = localStorage.getItem(key);
+            return item ? JSON.parse(item) : defaultValue;
+        } catch (err) {
+            console.error('خطا در بازیابی', err);
+            return defaultValue;
+        }
+    }
+};
+
+window.Storage = Storage;
