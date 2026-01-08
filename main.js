0e15ae15886646f3153b1978272f2614ed6ab5db
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-08 15:40:44 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service.worker.js')
-            .then(reg => console.log('✅ Service Worker ثبت شد:', reg.scope))
-            .catch(err => console.warn('❌ Service Worker ثبت نشد:', err));
-    });
-}
+
