6ab03e04eb7cc075308c0b278e027405c07cd9a5
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-08 14:42:54 +0330

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,11 @@
 
+const Engine = {
+    calc(expr){
+        try { return Function('"use strict";return ('+expr+')')(); }
+        catch(e){ return 'خطا'; }
+    },
+    todoList:[],
+    todoAdd(task){ this.todoList.push({task,done:false}); },
+    todoGetAll(){ return this.todoList; }
+};
+window.Engine=Engine;
